b75cf19d5d9e0fcd141b0f4bb61ef902179c4925
Mario <marioffdsw@outlook.com>
2015-11-19 21:12:53 -0500

    Prueba realizada, se logra llamar una funcion y recibir un valor de retorno

---
+++ OracleFunctionTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+using System.Data;
+
+namespace OracleFunctionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "User Id=web_legem;Password=web_legem;data source=ORCL";
+            OracleConnection connection = new OracleConnection { ConnectionString = connectionString };
+            connection.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.CommandText = "duplicar";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = connection;
+
+            OracleParameter retval = new OracleParameter("myretval", OracleDbType.Int32, 50);
+            retval.Direction = ParameterDirection.ReturnValue;
+            cmd.Parameters.Add(retval);
+
+            cmd.Parameters.Add(new OracleParameter("number", OracleDbType.Int32)).Value = 55;
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Return value is {0}", retval.Value);
+            connection.Close();
+            connection.Dispose();
+        }
+    }
+}
