22a2bfb236362cf528c815e021a76352c2aeb2cf
andi9010 <andres_solarteo@hotmail.com>
2015-12-09 14:48:56 -0500

    cambios ls-config

--- WebLegemClient/ls-conf.js
+++ WebLegemClient/ls-conf.js
@@ -4,8 +4,8 @@ var params = {
     port: 8181, // Set the server port. Defaults to 8080.
     host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0.
     //    root: ".", // Set root directory that's being server. Defaults to cwd.
-    open: '/build/#/', // When false, it won't load your browser by default.
-    ignore: '.\docs,.\src,.\.git', // comma-separated string for paths to ignore
+    open: '/src/#/', // When false, it won't load your browser by default.
+    ignore: '.\docs,.\git', // comma-separated string for paths to ignore
     wait: 0 // Waits for all changes, before reloading. Defaults to 0 sec.
 };
 
