271f4d470b548868f75e234b88e78fbf888ddb3b
Mario <marioffdsw@outlook.com>
2015-12-11 12:42:36 -0500

    cambios al controlador docs

--- WebLegemClient/src/app/gestion-documental/documento/documento.controller.js
+++ WebLegemClient/src/app/gestion-documental/documento/documento.controller.js
@@ -8,13 +8,24 @@
     DocumentoController.$inject = ["TipoDocumentoResource", "EntidadService", "FileUploader"];
     function DocumentoController(TipoDocumentoService, EntidadService, FileUploader) {
         var vm = this;
-        vm.documento = { docId: {}, asunto: "Asunto", ruta: "ruta", nombre: "nombre" }
+        vm.documento = {
+            docId: {
+                entidad: 1,
+                tipoDocumento: 1,
+                numero: "1025",
+                fechaPublicacion: "1999"
+            },
+            asunto: "Asunto",
+            ruta: "ruta",
+            nombre: "nombre"
+        }
+
         vm.prueba = "Prueba";
         vm.uploader = new FileUploader({ url: "http://localhost:50349/api/Files/" });
 
         vm.uploader.onSuccessItem = function (item, response, status, headers) {
-            vm.documento.ruta = response[0].path;
-            vm.documento.nombre = response[0].name;
+            vm.documento.rutaAlArchivo = response[0].path;
+            vm.documento.nombreDocumentoProcesado = response[0].name;
             console.log( response );
         };
 
