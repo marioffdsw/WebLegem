7cae6f7f2671cf1400a2446332c442e77e93305c
Mario <marioffdsw@gmail.com>
2016-01-30 19:41:24 -0500

    noticias, top 14, con funcion de descargar, falta la funcion de ver

--- WebLegemClient/src/app/homeController.js
+++ WebLegemClient/src/app/homeController.js
@@ -5,9 +5,9 @@
         .module("WebLegemApp")
         .controller("homeController", homeController);
 
-    homeController.$inject = ["$state"];
+    homeController.$inject = ["$state", "DocumentosResource"];
 
-    function homeController( $state) {
+    function homeController($state, DocumentosResource) {
         var vm = this;
         vm.$state = $state;        
 
@@ -21,13 +21,15 @@
         vm.icono_contraste = "ico-moon-o";
         vm.clase_contraste = "btn_tamano0";
         vm.val_contraste = 0;
-
+        vm.documentos = [];
 
         vm.loggeado = false;
         vm.LogOut = LogOut;
         vm.usuario = { username: "admin", password: "1234" }
 
-
+        DocumentosResource.query({ $top: 14 },function (data) {
+            vm.documentos = data;
+        });
 
         vm.opcionesNoLoggeado = [
             {
