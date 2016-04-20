a170c67689eef190935a2d938f5ce7d4a65d6c6c
Mario <marioffdsw@gmail.com>
2016-04-20 10:32:51 -0500

    se refactorizo la directiva eventos

---
+++ WebLegemClient/src/common/directives/wl-eventos/wl-eventos.directive.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("WebLegemApp")
+        .directive('ewl', function ewl () {
+            return {
+                restrict: 'A',
+                scope: true,
+                link: function (scope, element, attrs) {
+
+                    element.bind('load', function (e) {
+                        scope.$apply(attrs.wlLoad);
+                    })
+                    element.bind('mouseenter', function (e) {//onchange cambiar luego
+                        scope.$apply(attrs.wlHover);
+                    });
+
+                }
+            };
+        });
+})();
