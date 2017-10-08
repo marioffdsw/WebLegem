d1fd61856f0dd82df83e8eccf372ce5e8d259c35
AdryR <adry2507@gmail.com>
2017-10-08 17:50:09 -0500

    cambios adriana

--- WebLegemClient/src/app/app.core.constants.js
+++ WebLegemClient/src/app/app.core.constants.js
@@ -4,6 +4,6 @@
     angular
         .module("WebLegemApp")
         //.constant("serviceUrl", "/api");
-    .constant("serviceUrl", "http://localhost:50555/api");       
-        //.constant("serviceUrl", "http://192.168.0.17:50555/api");
+    //.constant("serviceUrl", "http://localhost:50555/api");       
+        .constant("serviceUrl", "http://192.168.0.2:50555/api");
 })();
