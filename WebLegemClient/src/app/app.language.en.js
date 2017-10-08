d7ec6ebcd46c09f1dda5b8fe01235cf302dc8110
AdryR <adry2507@gmail.com>
2017-10-08 16:57:59 -0500

    lenguaje ingles

--- WebLegemClient/src/app/app.language.en.js
+++ WebLegemClient/src/app/app.language.en.js
@@ -10,22 +10,114 @@
             spa: "Español",
             eng: "English",
             listadoTiposDocumento: "Document type list",
+            Inicio: "Start",
             //-------------Entidad---------//
             NombreEntidad: "Entity name",
             IngreseNombreEntidad: "Enter entity name ",
             TipoEntidad: "Entity type",
             ListaEntidades: "List of entities",
             ListaTipoEntidades: "Entity type list ",
+            BuscarEntidad: "Search Entity",
+            Error: "Error",
+            Entidades: "Entities",
             //-----------Tipos_entidad---------------------//
             NombreTipoEntidad: "Entity type name ",
             IngreseNombreTipoEntidad: "Enter the entity type name",
             IngreseCorreoElectronico: "Enter email",
-            CorreoNoValido:"Correo no valido",
+            CorreoNoValido: "Invalid mail",
+            BuscarTipoEntidad: "Search entity type",
+            DocumentosSoportados: "Documents supported",
             //-------------Tipo_documneto-------//
             NombreTipoDocumento: "Document type name",
+            BuscarDocumento: "Search document",
             //---------------------administracion-------------//
             TipoDocumento: "Document type",
             Entidad: "Entity",
+            ListaAnotaciones: "Annotation type list",
+            //----------control usuarios-------//
+            ArchivoCargadoServidor: "The file has been uploaded on the server",
+            ArchivoNoCargadoServidor: "The file has NOT uploaded on the server",
+            TomarFoto: "Take photo",
+            RepetirFoto: "Repeat photo",
+            GuardarFoto: "Save photo",
+            BorrarFoto: "Delete photo",
+            Fotografia: "Picture",
+            Nombres: "Names",
+            Apellidos: "Lastnames",
+            NombreUsuario: "Username",
+            Contrasena: "Password",
+            NivelSeguridad: "Security level",
+            ConfirmarContrasena: "Confirm password",
+            CorreoElectronico: "Email",
+            RolUsuario: "User role",
+            EstadoUsuario: "User state",
+            Activo: " Active",
+            Bloqueado: "Locked",
+            CerrarDialogo: "Close dialogue",
+            InformacionRol: "Role Information",
+            //---------cuenta-----------------//
+            ConfiguracionCuenta: "Account settings",
+            Guardar: "Save",
+            CorreoNoValido: "Invalid Mail",
+            //--------------recuperar clave------//
+            RecuperacionContrasena: "Password recovery",
+            IngreseNombreBotonVerificar: "Enter the user name with which you registered, and then click the check button",
+            IngreseCorreoBotonRecuperar: "Now Enter the email with which you registered, and click below the Recover button",
+            EmailEnviadoRevise: "An email has been sent to your email with the new password. Check your mail please",
+            Recuperar: "Get it back",
+            CuentaUsuario: "User account",
+            Verificar: "Check",
+            //-----------recuperar contraseña---------------//
+            Email: "E-mail",
+            Enviar: "Submit",
+            //---------------auditoria----------//
+            Roles: "ROLES",
+            Usuarios: "USERS",
+            Rol: "Role",
+            //---------------Busqueda-------------//
+            BusquedaAvanzada: "Advanced search",
+            Buscar: "Search",
+            EscribaBusqueda: "Type in your search (Example: Decree)",
+            documentosEncontrados: "Documents found",
+            //----------- ----asunto--------------//
+            CrearDocumento: "Create document",
+            Asunto: "Issue",
+            ProsesandoArchivo: "Processing file",
+            Atras: "Back",
+            Siguiente: "Next",
+            //------------informacion_documento------------//
+            IdentificacionDocumento: "Document identification",
+            Numero: "Number",
+            FechaPublicacion: "Publication date",
+            //------------------resultado_ocr--------------//
+            ResultadoOcr: "Upload successful",
+            Editar: "Edit",
+            Cancelar: "Cancel",
+            //------------------resultado---------------//
+            Resultado: "Result",
+            Trabajando: "Working",
+            DocumentoCreadoExito: "Document created successfully",
+            ErrorCrearDocumento: "Error creating document",
+            Regresar: "Return",
+            //------------------Subir_archivo--------//
+            SubirDocumento: "Upload Document",
+            EscogaUnArchivo: "Choose a file",
+            ArrastreloAqui: "Or drag it here.",
+            Anotacion: "Annotation",
+            ExisteAnota: "Annotation exists",
+            TipoArchivoNoSoportado: "File extension not supported",
+            SeleccioneArchivo: "Select a file",
+            //----------------login--------------//
+            IngresoSistema: "Login",
+            CompleteCampo: "Complete this field.",
+            CamposObligatorios: "* Required fields.",
+            Ingresar: "LOGIN",
+            OlvideContrasena: " I forgot my password",
+            NombreUsuarioI: "Username",
+            //------------------noticias-----------//
+            Legem: "Legem",
+            //----------control roles-----------------------//
+            Permisos:"Permits",
             //--------------crear anotacion----------//
             SeleccionarDocumentoAnota:"Select document to record",
             NoEncontroNingunDocumentoConIdentificacion: "No document found with this identification",
@@ -34,12 +126,14 @@
             NoEcontroNingunDocumentoIdentificacionDeseaCrear:"No document found with this identification, You want to create a",
             NuevoDocumento:"New document",
             TipoAnotacion: "Annotation type",
-            EscribaEntidadEmisora: "Type Issuing entity",
-            DescripcionDeAnotacion:"Description Annotation",
-            CrearAnotacion: "Create Annotation",
-          
-            //------------Editar anotacion------//
-            BuscarDocumento: "Search document",
+            EscribaEntidadEmisora: "Type issuing entity",
+            DescripcionDeAnotacion:"Description annotation",
+            CrearAnotacion: "Create annotation",
+            SehaCreadoAnotacion: "An annotation has been created",
+            CompleteDatosCrearAnotacion:"Complete the data to create an annotation",
+            SeEncontroN: "It was found",
+            ResultadoBusqueda:"Search results",
+            //------------Editar anotacion------//    
             VistaPreviaDocumento:"Document preview ",
             GuardarCambios: "Save changes",
             VistaPrevia: "Preview",
@@ -47,11 +141,10 @@
             ModificarAnotacionSeguro: "it will edit this annotation, Are you sure?",
             //--------------Eliminar anotacion----------//
             SeleccioneDocumentoEliminarAnotacione: "Select the document you want to delete annotations",
-            EliminarAnotacion: "Delete Annotation",
+            EliminarAnotacion: "Delete annotation",
             EliminarAnotacionSeguro:"It will delete this annotation, Are you sure?",
             Confirmar: "Confirm",
-            EliminadoAnotacionesExito: "Annotations has been deleted successfully",
-             
+            EliminadoAnotacionesExito: "Annotations has been deleted successfully",             
             //------------anotacion mayusculas-----------------//
             CrearAnotacionM: "CREATE ANOTATION ",
             EliminarAnotacionM: "DELETE ANOTATION",
@@ -74,113 +167,32 @@
             RolPasivoAnotacion: "Passive Role annotation",
             Anadir: "Add",
             Remover: "Remove",
-            NombreTipoAnotacion: "Name Annotation type",
+            NombreTipoAnotacion: "Name annotation type",
             IngreseNombreTipoAnotacion: "Enter name of the annotation type",
             IngreseRaizTipoAnotacion: "Enter the root of the notation type",
-            
+            RaizTipoAnotacion: "Root annotation type",
             //--------------lista anotaciones -----------------//
             NAnotacionesEncontradas: "N annotations found",
             Detalles: "Details",
             QuitarSeleccion:"Delete selection",
-            Descripcion:"Description",
+            Descripcion: "Description",
+            AnoExpedicion: "Year of issue",
+            SeleccioneDucumentoAnotar:"Select the document you want to annotate",
             CompleCamposRealizarAnotacion: "Complete the fields to make the annotation",
             DescripcionAnotacion: "Annotation description",
-            //---------------crear anotacion ----------------------//
-            AnoExpedicion: "Year of issue",
-            verificar: "Check",
-            seleccioneDocumentoAnotar: "Select the document that you want to annotate",           
-            //---------------------pregunta anadir anotaciones---------------------//
-            DeseaAnadirAnotacionesDocumento: "¿You want to add annotations to this document?",
-            //------------------seleccionar anotacion -------------------------------//           
-            ExistenPresenteDocumento: "There are n possible documents which records this document",             
-            Aceptar: "OK",            
-            Emitido: "Issued by",
+            //---------------crear anotacion ----------------------//            
             seleccionarDocumento: "Select document",
             EntidadEmisora: "Issuing entity",
-            AnoPublicacion: "Year of publication",            
-            FechaPublicacion: "Publication date",
-            //----------control usuarios-------//
-            ArchivoCargadoServidor: "The file has been uploaded on the server",
-            ArchivoNoCargadoServidor: "The file has NOT uploaded on the server",
-            TomarFoto: "Take photo",
-            RepetirFoto: "Repeat photo",
-            GuardarFoto: "Save photo",
-            BorrarFoto: "Delete photo",
-            Fotografia: "Picture",
-            Nombres: "Names",
-            Apellidos: "Lastnames",
-            NombreUsuario: "Username",
-            Contrasena: "Password",
-            NivelSeguridad: "Security level",
-            ConfirmarContrasena: "Confirm password",
-            CorreoElectronico: "Email",
-            RolUsuario: "User role",
-            EstadoUsuario: "User state",
-            Activo: " Active",
-            Bloqueado: "Locked",
-            //---------cuenta-----------------//
-            ConfiguracionCuenta: "Account settings",
-            Guardar: "Save",
-            //--------------recuperar clave------//
-            RecuperacionContrasena: "Password recovery",
-            IngreseNombreBotonVerificar: "Enter the user name with which you registered, and then click the check button",
-            IngreseCorreoBotonRecuperar: "Now Enter the email with which you registered, and click below the Recover button",
-            EmailEnviadoRevise: "An email has been sent to your email with the new password. Check your mail please",
-            Recuperar: "Get it back",
-            CuentaUsuario: "User account",
-            Verificar:"Check",
-            //-----------recuperar contraseña---------------//
-            Email: "E-mail",
-            Enviar: "Submit",
-            //---------------auditoria----------//
-            Roles: "ROLES",
-            Usuarios: "USERS",
-            //---------------Busqueda-------------//
-            BusquedaAvanzada: "Advanced search",
-            Buscar: "Search",
-            EscribaBusqueda: "Type in your search (Example: Decree)",
-            documentosEncontrados: "Documents found",
-            //----------- ----asunto--------------//
-            CrearDocumento: "Create document",
-            Asunto: "Issue",
-            ProsesandoArchivo: "Processing file",
-            Atras: "Back",
-            Siguiente: "Next",
-            //------------informacion_documento------------//
-            IdentificacionDocumento: "Document identification",
-            Numero: "Number",            
-            //------------------resultado_ocr--------------//
-            ResultadoOcr: "OCR result",
-            Editar: "Edit",
-            Cancelar: "Cancel",
-            //------------------resultado---------------//
-            Resultado: "Result",
-            Trabajando: "Working",
-            DocumentoCreadoExito: "Document created successfully",
-            ErrorCrearDocumento: "Error creating document",
-            Regresar: "Return",
-            //------------------Subir_archivo--------//
-
-            
-            SubirDocumento: "Upload Document",
-            EscogaUnArchivo: "Choose a file",
-            ArrastreloAqui: "Or drag it here.",
-            Anotacion: "Annotation",
-            ExisteAnota: "Annotation exists",
-            TipoArchivoNoSoportado: "File extension not supported",
-            SeleccioneArchivo: "Select a file",
-
-
-            //----------------login--------------//
-            IngresoSistema: "Login",
-            CompleteCampo: "Complete this field.",
-            CamposObligatorios: "* Required fields.",
-            Ingresar: "LOGIN",
-            OlvideContrasena: " I forgot my password",
-            NombreUsuarioI:"Username",            
-            //------------------noticias-----------//
-            Legem: "Legem",                     
-                //------------------------footer--------------------//
+            AnoPublicacion: "Year of publication",
+            //------------lista ano publicaciones-----------------//
+             //---------------------pregunta anadir anotaciones---------------------//
+            DeseaAnadirAnotacionesDocumento: "¿You want to add annotations to this document?",
+            //------------------seleccionar anotacion -------------------------------//   
+            ExistePresenteDocumento:"Exist",
+            ExistenPresenteDocumento: "There are n possible documents which records this document",
+            Emitido: "Issued by",
+            Aceptar: "OK",
+            //------------------------footer--------------------//
             Contactenos: "Contact",
             UniversidadNarino: "University of Nariño",
             Torobajo: "Torobajo - Street 18 Crr 50",
@@ -195,13 +207,13 @@
             EscribaNumeroDocumento: "Write document number",
             EscribaAnoPublicacion: "Write year of publication",
             Fecha: "Date",
-            Relevancia: "Relevance",
-            //---roless----
+            Relevancia: "Relevance",               
+            //------------------filtro----------//
             Filtro: "Filter",
             //---------------wlwgem-news---------//
             Ninguna: "Any",
             ActualidadDocumental: "Current documentary",
-            //-----componentes-------//
+            //--------------header-----------//
             Menu: "Menu",
             Componentes: "Components",
             ConfigurarCuenta: "Account settings",
@@ -210,33 +222,62 @@
             Idioma: "Language",
             Espanol: "Spanish",
             Ingles: "English",
-            
             Irarriba: "Go up",
-            //---------------place--------//
-           
-            NoEntontroDocumento: "No document found with that identification",
-            IngreseNombre: "Enter name",               
+            //------------------place-------------------//
+            NoEncontroDocumento: "No document was found with that identification",
+            IngreseNombre: "Enter name",
             //----- info anotaciones-----//
-            Anotaciones: "Annotations",            
+            Anotaciones: "Annotations",
             EncuentrAnotado: "It is annotated by",
-            del: "of the",            
+            del: "of the",
             Deroga: "Repeals",
-            //-------Permisos-------//
+            //--------------------confirmacion-------------------//
             PermitirTodo: "Allow all",
             NoPermitido: "Not allowed",
             //--------adminCrud----------------//
-            Nuevo:"New",
+            Nuevo: "New",
             Eliminar: "Delete",
             //-------------rolDirective--------//
-            permisos: "Permits",
+            permisos:"Permissions",
             Modulo: "Module",
             Lectura: "Reading",
             Escritura: "Writing",
             Edicion: "Edition",
             Eliminacion: "Removal",
             //----subir documento-----//
-            SeleccioneOpcion:"Select an option",
-            
+            SeleccioneOpcion: "Select an option",
+            Notificar: "Notify",
+
+             //---------------------lista de entidades----------//
+            ErrorCargarDatos: "Error loading data",
+            //-------------------------procesando--------------//
+            SeCreoAnotacionExito: "The annotation was successfully created",
+            LamentablementeNoPudoRealizarOperacion: "Unfortunately the operation could not be performed",
+
+            //------------------href title- y alt -----------------//
+            Facebook: "Facebook",
+            Twitter: "Twitter",
+            FotografiaUsuario: "User Photography",
+            //---------------------lista usuarios-----------------//
+            QuitarFiltro: "Remove filter",
+            //-------------------principal------------//
+            LogoAcreditacion: "Logo accreditation Universidad de Nariño",
+            LogoPlaneacion: "Logo planning Universidad de nariño",
+            EscudoAnos: "Shield 111 years ",
+            //---------------lista de anotaciones------------------//
+            DetalleAnotacion: "Annotation Detail",
+            //---------------------------lista tipo de anotaciones---------//
+            ListaTipoAnotacion: "Annotation type list", 
+            //---------------admin-------------------------//
+            EstaSeguroDeseaEliminar: "Are you sure you want to delete",
+            //--------------------asociar documento-----------//                                
+            AsociarDocumento: "Associate document",
+            //-----------------fotografia-------------------//                                                                                   
+            CerrarCamara: "Close camera",
+
+
+
+
             //------------------OJO
 
             arregloOpciones: [
@@ -271,6 +312,5 @@
                 condicion: "true"
             }
             ]
-
-        });
+         });
 })();
